47e5fd5a38d6f8315c9ffdf4b9c31b119cc9c94b
RinPoetry <doraemonmail@qq.com>
2023-11-23 15:02:49 +0800

    Brunch dev test.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+void branchDev()
+{
+    std::cout << "Brunch dev here." << std::endl;
+}
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    branchDev();
     return 0;
 }
